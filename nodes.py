d64b322716389220805731f54039065ccb404a66
OpenSource CNC <80465167+opensourcecnc@users.noreply.github.com>
2026-01-29 19:02:30 +0200

    Add files via upload

--- nodes.py
+++ nodes.py
@@ -8,6 +8,7 @@ from contextlib import nullcontext
 import comfy.model_management as mm
 from comfy.utils import ProgressBar, load_torch_file
 import folder_paths
+import numpy as np
 
 from .depth_anything_v2.dpt import DepthAnythingV2
 
@@ -171,16 +172,86 @@ https://depth-anything-v2.github.io
 
         if depth_out.shape[1] != final_H or depth_out.shape[2] != final_W:
             depth_out = F.interpolate(depth_out.permute(0, 3, 1, 2), size=(final_H, final_W), mode="bilinear").permute(0, 2, 3, 1)
-        depth_out = (depth_out - depth_out.min()) / (depth_out.max() - depth_out.min())*65025.0
+        depth_out = (depth_out - depth_out.min()) / (depth_out.max() - depth_out.min())
+        depth_out = torch.clamp(depth_out, 0, 1)
         if da_model['is_metric']:
             depth_out = 1 - depth_out
         return (depth_out,)
+
+class DepthAnything_V2_16Bit:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "da_model": ("DAMODEL", ),
+            "images": ("IMAGE", ),
+            },
+        }
     
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES =("image",)
+    FUNCTION = "process"
+    CATEGORY = "DepthAnythingV2"
+    DESCRIPTION = """
+https://depth-anything-v2.github.io
+"""
+
+    def process(self, da_model, images):
+        device = mm.get_torch_device()
+        offload_device = mm.unet_offload_device()
+        model = da_model['model']
+        dtype=da_model['dtype']
+        
+        B, H, W, C = images.shape
+
+        #images = images.to(device)
+        images = images.permute(0, 3, 1, 2)
+
+        orig_H, orig_W = H, W
+        if W % 14 != 0:
+            W = W - (W % 14)
+        if H % 14 != 0:
+            H = H - (H % 14)
+        if orig_H % 14 != 0 or orig_W % 14 != 0:
+            images = F.interpolate(images, size=(H, W), mode="bilinear")
+        
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        normalized_images = normalize(images)
+        pbar = ProgressBar(B)
+        out = []
+        model.to(device)
+        autocast_condition = (dtype != torch.float32) and not mm.is_device_mps(device)
+        with torch.autocast(mm.get_autocast_device(device), dtype=dtype) if autocast_condition else nullcontext():
+            for img in normalized_images:
+                depth = model(img.unsqueeze(0).to(device))
+                depth = (depth - depth.min()) / (depth.max() - depth.min()) * 65025.0
+                depth = depth.astype(np.uint16)
+                out.append(depth.cpu())
+                pbar.update(1)
+            model.to(offload_device)
+            depth_out = torch.cat(out, dim=0)
+            depth_out = depth_out.unsqueeze(-1).repeat(1, 1, 1, 3).cpu().float()
+        
+        final_H = (orig_H // 2) * 2
+        final_W = (orig_W // 2) * 2
+
+        
+
+        if depth_out.shape[1] != final_H or depth_out.shape[2] != final_W:
+            depth_out = F.interpolate(depth_out.permute(0, 3, 1, 2), size=(final_H, final_W), mode="bilinear").permute(0, 2, 3, 1)
+        depth_out = (depth_out - depth_out.min()) / (depth_out.max() - depth_out.min())
+        depth_out = torch.clamp(depth_out, 0, 1)
+        if da_model['is_metric']:
+            depth_out = 1 - depth_out
+        return (depth_out,)
+
+
 NODE_CLASS_MAPPINGS = {
     "DepthAnything_V2": DepthAnything_V2,
+    "DepthAnything_V2_16Bit": DepthAnything_V2_16Bit,
     "DownloadAndLoadDepthAnythingV2Model": DownloadAndLoadDepthAnythingV2Model
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DepthAnything_V2": "Depth Anything V2",
+    "DepthAnything_V2_16Bit": "Depth Anything V2 16 Bit",
     "DownloadAndLoadDepthAnythingV2Model": "DownloadAndLoadDepthAnythingV2Model"
 }
